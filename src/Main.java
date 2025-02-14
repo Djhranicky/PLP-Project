ea6d18cc131f9abd7de166f41bb9dc07ce525d54
David Hranicky <djh30000@gmail.com>
2025-02-13 19:40:33 -0500

    Update to use hard-coded test file

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
+import java.io.IOException;
+
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 public class Main {
-    public static void main(String args[]) {
-        CharStream inputStream = CharStreams.fromString("program HelloWorld;\r\n" + //
-                        "uses crt;\r\n" + //
-                        "\r\n" + //
-                        "(* Here the main program block starts *)\r\n" + //
-                        "begin\r\n" + //
-                        "   writeln('Hello, World!');\r\n" + //
-                        "   readkey;\r\n" + //
-                        "end.");
+    public static void main(String args[]) throws IOException {
+        CharStream inputStream = CharStreams.fromFileName("test-pascal.pas");
 
         DelphiLexer delphiLexer = new DelphiLexer(inputStream);
         CommonTokenStream tokens = new CommonTokenStream(delphiLexer);
