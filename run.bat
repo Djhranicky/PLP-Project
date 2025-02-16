98be9b95e30e39ccacc2c63304885b8809c8e508
David Hranicky <djh30000@gmail.com>
2025-02-16 09:38:58 -0500

    Fix run.bat to compile and run project thorugh Main.java

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 echo off
 set arg1=%1
-antlr4 delphi.g4
-javac delphi*.java
-grun.bat delphi program %arg1% -gui &
+antlr4 grammar/delphi.g4 -o ./srcs
+javac srcs/delphi*.java
+java srcs/Main.java
+::grun.bat delphi program %arg1% -gui &
