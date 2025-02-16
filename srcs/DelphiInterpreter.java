2eec93f3fc957cd1f6cbc5bc79be5fa17d879339
David Hranicky <djh30000@gmail.com>
2025-02-16 09:30:41 -0500

    Re-add erased changes as starting point

--- srcs/DelphiInterpreter.java
+++ srcs/DelphiInterpreter.java
@@ -1,13 +1,31 @@
-
-
 public class DelphiInterpreter extends DelphiBaseListener {
     @Override
+    public void enterProgramHeading(DelphiParser.ProgramHeadingContext ctx) {
+        System.out.println("Running program: " + ctx.identifier().getText());
+    }
+
+    @Override
+    public void enterClassDecl(DelphiParser.ClassDeclContext ctx) {
+        System.out.println("Declaring class: " + ctx.identifier(0).getText());
+    }
+
+    @Override
     public void enterProgram(DelphiParser.ProgramContext ctx) {
-        System.out.println("enterProgram: " + ctx.getText());
+        // System.out.println("enterProgram: " + ctx.getText());
     }
 
     @Override
     public void exitProgram(DelphiParser.ProgramContext ctx) {
-        System.out.println("exitProgram: " + ctx.getText());
+        // System.out.println("exitProgram: " + ctx.getText());
+    }
+
+    @Override
+    public void enterIdentifierList(DelphiParser.IdentifierListContext ctx) {
+        // System.out.println("enterIdentifierList: " + ctx.getText());
+    }
+
+    @Override
+    public void enterIdentifier(DelphiParser.IdentifierContext ctx) {
+        // System.out.println("enterIdentifier: " + ctx.getText());
     }
 }
