084ef5c7e40c54677b51719a62488c01b8deedaa
Paraman Galipalli <96441126+iTzZe4lous@users.noreply.github.com>
2025-02-15 20:49:57 -0500

    Update test-pascal.pas

--- test-pascal.pas
+++ test-pascal.pas
@@ -1,8 +1,21 @@
-program HelloWorld;
-uses crt;
+PROGRAM CompleteClass;
+CLASS Rectangle
+    PRIVATE
+        width, height: Real;
+    PUBLIC
+        CONSTRUCTOR Create(w, h: Real);
+        FUNCTION GetArea: Real;
 
-(* Here the main program block starts *)
-begin
-   writeln('Hello, World!');
-   readkey;
-end. 
+    FUNCTION GetArea: Real;
+    BEGIN
+        GetArea := width * height;
+    END;
+    
+    CONSTRUCTOR Create(w, h: Real);
+    BEGIN
+        width := w;
+        height := h;
+    END;
+END;
+BEGIN
+END.
