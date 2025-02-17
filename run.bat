900bd69d93d5c4dd18c834f59c17e6f6955cdb2b
iTzZe4lous <paraman.galipalli@gmail.com>
2025-02-16 22:02:16 -0500

    Updated run file to compile .java files together and run in Main

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 echo off
 set arg1=%1
 antlr4 grammar/delphi.g4 -o ./srcs
-javac srcs/delphi*.java
-java srcs/Main.java
+javac srcs/*.java
+java -cp srcs Main
 ::grun.bat delphi program %arg1% -gui &
